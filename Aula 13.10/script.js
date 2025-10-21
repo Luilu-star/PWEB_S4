18f3c5268ba59ed67502a59eadc2b5f5e0726d25
Luilu-star <luiotv2302@gmail.com>
2025-10-21 11:57:26 -0300

    introdução à manipulação DOM

---
+++ Aula 13.10/script.js
@@ -0,0 +1,16 @@
+const title = document.getElementById('titulo');
+
+let input = document.createElement("p");
+
+input.innerHTML = '<p>aiaiaiaiai</p>'
+
+document.body.append(input)
+
+let img = document.createElement("img");
+
+img.setAttribute("src", "LocaBus-removebg.png");
+
+img.classList.add('destaque')
+
+
+document.body.append(img);
